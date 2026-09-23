56b61cf0246a8f82780e9d8ce324bab6a1d0e634
umimpul <usanchez@ecuelalosandes.edu.ar>
2026-09-23 16:59:48 -0300

    etapa 3

--- examen.py
+++ examen.py
@@ -1,6 +1,6 @@
 # Examen práctico - Sistema de pedidos del kiosco
-# Nombre y apellido:
-# Curso:
+# Nombre y apellido: uma sanchez
+# Curso:2/2
 #
 # IMPORTANTE:
 # Resolver el programa siguiendo las etapas indicadas en el README.md.
@@ -16,6 +16,8 @@
 # Crear las listas de productos y precios.
 # Pedir los datos del cliente.
 
+listas_productos = ("caramelos 10 $, puflitos 20$, chupetines 70$, chicles 5$")
+input("nombre y apellido:")
 
 # =========================
 # ETAPA 2 - COMPRAS
@@ -24,12 +26,21 @@
 # Mostrar el menú y procesar la opción seleccionada.
 # Utilizar las listas para obtener producto y precio.
 
-
-# =========================
-# ETAPA 3 - CICLO PRINCIPAL
-# =========================
-
-# Modificar el programa para que continúe funcionando
+print("lista_productos:", listas_productos)
+print("ingrese el producto que quiera comprar:")
+producto_seleccionado = ("caramelos 10 $, puflitos 20$, chupetines 70$, chicles 5$")
+producto_seleccionado = input("caramelos 10 $, puflitos 20$, chupetines 70$, chicles 5$")
+if producto_seleccionado == "caramelos":
+    print("el precio es 10 $")
+elif producto_seleccionado == "puflitos":
+    print("el precio es 20 $")
+elif producto_seleccionado == "chupetines":
+    print("el precio es 70 $")
+elif producto_seleccionado == "chicles":
+    print("el precio es 5 $")
+else:
+    print("producto no válido")
+   
 # hasta que el usuario decida finalizar la compra.
 
 
